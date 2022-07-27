6c48bee8f00a25115032d3e807886bffafe7e32a
mahendra2448 <mahendrasyathi@gmail.com>
2022-07-27 23:57:07 +0700

    Add fillable fields

--- app/Models/History.php
+++ app/Models/History.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class History extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'month_id',
+        'year',
+        'achievements',
+        'leave_days',
+        'late_morning',
+        'late_evening',
+        'late_minutes',
+        'additional_insentives',
+        'additional_insentives_tax',
+        'bpjs_kes_amount',
+        'bpjs_kenaker_amount',
+        'loan',
+        'other_deduction',
+        'tax',
+        'application',
+        'allowance',
+        'total_salary',
+        'notes',
+        'created_by', //by 'System'
+        'saved_date'
+    ];
 }
--- app/Models/MonthlyData.php
+++ app/Models/MonthlyData.php
@@ -10,4 +10,28 @@ class MonthlyData extends Model
     use HasFactory;
 
     protected $table = 'monthly_update_data';
+
+    protected $fillable = [
+        'employee_id',
+        'month_id',
+        'year',
+        'achievements',
+        'leave_days',
+        'late_morning',
+        'late_evening',
+        'late_minutes',
+        'additional_insentives',
+        'additional_insentives_tax',
+        'bpjs_kes_amount',
+        'bpjs_kenaker_amount',
+        'loan',
+        'other_deduction',
+        'tax',
+        'application',
+        'allowance',
+        'total_salary',
+        'notes',
+        'created_by',
+        'updated_by'
+    ];
 }
