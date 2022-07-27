216c913491e0d132229513873f60e416f3df7d43
mahendra2448 <mahendrasyathi@gmail.com>
2022-07-27 23:53:51 +0700

    Add fillable fields

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'nik',
+        'email',
+        'phone',
+        'nip',
+        'position_id',
+        'main_salary',
+        'account_bank',
+        'account_name',
+        'account_number',
+        'workdays',
+        'parking_status',
+        'parking_amount',
+        'insurance_amount',
+        'bpjs_kes_amount',
+        'bpjs_kenaker_amount',
+        'tax',
+        'created_by',
+        'updated_by'
+    ];
 }
